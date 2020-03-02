e4e92795616a1f792c052e3e5d3321bf6fc16383
Naomi Aro <naomiaro@gmail.com>
2020-03-02 17:23:44 +0200

    update theme

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -2,7 +2,7 @@ import { addDecorator } from '@storybook/react';
 import { withThemesProvider } from 'storybook-addon-styled-component-theme';
 
 const base = {
-  waveOutlineColor: 'white',
+  waveOutlineColor: 'black',
   waveFillColor: 'green',
   waveProgressColor: 'orange',
   waveHeight: 80,
@@ -11,5 +11,18 @@ const base = {
   controlWidth: 0,
 };
 
-const themes = [Object.assign({}, base, { name: 'Base' })];
+const reverse = {
+  waveOutlineColor: 'green',
+  waveFillColor: 'black',
+  waveProgressColor: 'orange',
+  waveHeight: 80,
+  timeColor: 'grey',
+  timeScaleHeight: 10,
+  controlWidth: 0,
+};
+
+const themes = [
+  Object.assign({}, reverse, { name: 'Reverse' }),
+  Object.assign({}, base, { name: 'Base' }),
+];
 addDecorator(withThemesProvider(themes));
