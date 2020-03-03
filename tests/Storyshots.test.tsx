e2a60a605b7f1500c6a14d88327b82f27c433ded
Naomi Aro <naomiaro@gmail.com>
2020-03-03 19:54:14 +0200

    get snapshots working

--- tests/Storyshots.test.tsx
+++ tests/Storyshots.test.tsx
@@ -1,4 +1,10 @@
 import initStoryshots from '@storybook/addon-storyshots';
 import 'jest-styled-components';
+import { mount } from 'enzyme';
+import { createSerializer } from 'enzyme-to-json';
 
-initStoryshots();
+initStoryshots({
+  renderer: mount,
+  //@ts-ignore
+  snapshotSerializers: [createSerializer()],
+});
