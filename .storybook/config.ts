4eec3479d205aa5af562673f4a5190cfeead6fb1
Naomi Aro <naomiaro@gmail.com>
2020-03-03 21:40:42 +0200

    storysnap not working with theme addon

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,28 +1,28 @@
-import { addDecorator } from '@storybook/react';
-import { withThemesProvider } from 'storybook-addon-styled-component-theme';
+// import { addDecorator } from '@storybook/react';
+// import { withThemesProvider } from 'storybook-addon-styled-component-theme';
 
-const base = {
-  waveOutlineColor: 'black',
-  waveFillColor: 'green',
-  waveProgressColor: 'orange',
-  waveHeight: 80,
-  timeColor: 'grey',
-  timeScaleHeight: 10,
-  controlWidth: 0,
-};
+// const base = {
+//   waveOutlineColor: 'black',
+//   waveFillColor: 'green',
+//   waveProgressColor: 'orange',
+//   waveHeight: 80,
+//   timeColor: 'grey',
+//   timeScaleHeight: 10,
+//   controlWidth: 0,
+// };
 
-const reverse = {
-  waveOutlineColor: 'green',
-  waveFillColor: 'black',
-  waveProgressColor: 'orange',
-  waveHeight: 80,
-  timeColor: 'grey',
-  timeScaleHeight: 10,
-  controlWidth: 0,
-};
+// const reverse = {
+//   waveOutlineColor: 'green',
+//   waveFillColor: 'black',
+//   waveProgressColor: 'orange',
+//   waveHeight: 80,
+//   timeColor: 'grey',
+//   timeScaleHeight: 10,
+//   controlWidth: 0,
+// };
 
-const themes = [
-  Object.assign({}, reverse, { name: 'Reverse' }),
-  Object.assign({}, base, { name: 'Base' }),
-];
-addDecorator(withThemesProvider(themes));
+// const themes = [
+//   Object.assign({}, reverse, { name: 'Reverse' }),
+//   Object.assign({}, base, { name: 'Base' }),
+// ];
+// addDecorator(withThemesProvider(themes));
--- tests/Storyshots.test.tsx
+++ tests/Storyshots.test.tsx
@@ -1,38 +1,6 @@
-import 'jest-styled-components';
-// import initStoryshots, {
-//   Stories2SnapsConverter,
-// } from '@storybook/addon-storyshots';
-// import { mount } from 'enzyme';
-// import toJson from 'enzyme-to-json';
+// TODO - this isn't working when using addon `storybook-addon-styled-component-theme`
 
-// initStoryshots({
-//   asyncJest: true, // this is the option that activates the async behaviour
-//   test: ({
-//     story,
-//     context,
-//     done, // --> callback passed to test method when asyncJest option is true
-//   }) => {
-//     const converter = new Stories2SnapsConverter();
-//     const snapshotFilename = converter.getSnapshotFileName(context);
-//     const storyElement = story.render();
+// import initStoryshots from '@storybook/addon-storyshots';
+// import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
-//     // mount the story
-//     const tree = mount(storyElement);
-
-//     // wait until the mount is updated, in our app mostly by Relay
-//     // but maybe something else updating the state of the component
-//     // somewhere
-//     const waitTime = 1;
-//     setTimeout(() => {
-//       if (snapshotFilename) {
-//         expect(toJson(tree.update())).toMatchSpecificSnapshot(snapshotFilename);
-//       }
-
-//       done();
-//     }, waitTime);
-//   },
-//   // other options here
-// });
-
-import initStoryshots from '@storybook/addon-storyshots';
-initStoryshots();
+// initStoryshots({ suite: 'Image storyshots', test: imageSnapshot() });
