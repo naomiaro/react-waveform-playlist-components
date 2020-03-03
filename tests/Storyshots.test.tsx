ac6ae726185a8ff330bb608ea55078d8e7c3d9e0
Naomi Aro <naomiaro@gmail.com>
2020-03-03 20:54:29 +0200

    snapshots still null

--- tests/Storyshots.test.tsx
+++ tests/Storyshots.test.tsx
@@ -1,10 +1,38 @@
-import initStoryshots from '@storybook/addon-storyshots';
 import 'jest-styled-components';
-import { mount } from 'enzyme';
-import { createSerializer } from 'enzyme-to-json';
+// import initStoryshots, {
+//   Stories2SnapsConverter,
+// } from '@storybook/addon-storyshots';
+// import { mount } from 'enzyme';
+// import toJson from 'enzyme-to-json';
 
-initStoryshots({
-  renderer: mount,
-  //@ts-ignore
-  snapshotSerializers: [createSerializer()],
-});
+// initStoryshots({
+//   asyncJest: true, // this is the option that activates the async behaviour
+//   test: ({
+//     story,
+//     context,
+//     done, // --> callback passed to test method when asyncJest option is true
+//   }) => {
+//     const converter = new Stories2SnapsConverter();
+//     const snapshotFilename = converter.getSnapshotFileName(context);
+//     const storyElement = story.render();
+
+//     // mount the story
+//     const tree = mount(storyElement);
+
+//     // wait until the mount is updated, in our app mostly by Relay
+//     // but maybe something else updating the state of the component
+//     // somewhere
+//     const waitTime = 1;
+//     setTimeout(() => {
+//       if (snapshotFilename) {
+//         expect(toJson(tree.update())).toMatchSpecificSnapshot(snapshotFilename);
+//       }
+
+//       done();
+//     }, waitTime);
+//   },
+//   // other options here
+// });
+
+import initStoryshots from '@storybook/addon-storyshots';
+initStoryshots();
