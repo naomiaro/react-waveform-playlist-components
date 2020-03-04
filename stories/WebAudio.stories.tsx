869f34be5b48db3805709a252abe7ffbb9769ff0
Naomi Aro <naomiaro@gmail.com>
2020-03-04 21:03:16 +0200

    shorten webaudio to knobs

--- stories/WebAudio.stories.tsx
+++ stories/WebAudio.stories.tsx
@@ -3,16 +3,6 @@ import { withKnobs, select, boolean, number } from '@storybook/addon-knobs';
 import { WebAudioProvider } from '../src/contexts/WebAudio';
 import { StyledChannel } from '../src/components/Channel/Channel';
 import { StyledTrack } from '../src/components/Track';
-import {
-  Button,
-  ButtonGroup,
-  Controls,
-  Header,
-  VolumeDownIcon,
-  VolumeUpIcon,
-  VolumeSlider,
-  VolumeSliderWrapper,
-} from '../src/components/TrackControls';
 
 export default {
   title: 'WebAudio',
@@ -20,76 +10,6 @@ export default {
 };
 
 export const Default = () => (
-  <WebAudioProvider source={'/audio/Vocals30.mp3'}>
-    {(peaks, bits, length) => (
-      <StyledTrack numChannels={peaks.length} controls={null}>
-        {peaks.map((data, index) => (
-          <StyledChannel
-            key={index}
-            index={index}
-            data={data}
-            bits={bits}
-            length={length}
-          />
-        ))}
-      </StyledTrack>
-    )}
-  </WebAudioProvider>
-);
-
-export const MultiChannel = () => (
-  <WebAudioProvider source={'/audio/Vocals30.mp3'} showMultiChannel={true}>
-    {(peaks, bits, length) => (
-      <StyledTrack numChannels={peaks.length} controls={null}>
-        {peaks.map((data, index) => (
-          <StyledChannel
-            key={index}
-            index={index}
-            data={data}
-            bits={bits}
-            length={length}
-          />
-        ))}
-      </StyledTrack>
-    )}
-  </WebAudioProvider>
-);
-
-export const WithControls = () => (
-  <WebAudioProvider source={'/audio/Vocals30.mp3'} showMultiChannel={true}>
-    {(peaks, bits, length) => (
-      <StyledTrack
-        numChannels={peaks.length}
-        controls={
-          <Controls>
-            <Header>Track 1</Header>
-            <ButtonGroup>
-              <Button>Mute</Button>
-              <Button>Solo</Button>
-            </ButtonGroup>
-            <VolumeSliderWrapper>
-              <VolumeDownIcon />
-              <VolumeSlider />
-              <VolumeUpIcon />
-            </VolumeSliderWrapper>
-          </Controls>
-        }
-      >
-        {peaks.map((data, index) => (
-          <StyledChannel
-            key={index}
-            index={index}
-            data={data}
-            bits={bits}
-            length={length}
-          />
-        ))}
-      </StyledTrack>
-    )}
-  </WebAudioProvider>
-);
-
-export const WithKnobs = () => (
   <WebAudioProvider
     source={select(
       'source',
