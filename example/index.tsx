f1ef82661b9f34a13f982a42558a963cda94d885
Naomi Aro <naomiaro@gmail.com>
2020-03-05 19:24:44 +0200

    more files

--- example/index.tsx
+++ example/index.tsx
@@ -1,17 +1,21 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
 
 import { Playout } from '../src/playout';
 
 async function test1() {
-  const playout = new Playout(['/media/audio/Vocals30.mp3']);
+  const playout = new Playout([
+    '/media/audio/Vocals30.mp3',
+    '/media/audio/Guitar30.mp3',
+    '/media/audio/PianoSynth30.mp3',
+    '/media/audio/BassDrums30.mp3',
+  ]);
   try {
     await playout.load();
     playout.play();
 
-    setTimeout(playout.stop.bind(playout), 1000);
+    setTimeout(playout.stop.bind(playout), 5000);
   } catch (e) {
     console.log(e);
   }
