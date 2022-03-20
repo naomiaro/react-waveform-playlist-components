5523a017e1e18d3fc58779dfe3ef23036e1eb873
Naomi Aro <naomiaro@gmail.com>
2022-03-20 14:15:29 -0700

    tweak tests

--- test/storyshots.test.ts
+++ test/storyshots.test.ts
@@ -1,92 +1,53 @@
 import { join } from 'path';
 import initStoryshots from '@storybook/addon-storyshots';
 import { imageSnapshot, Context } from '@storybook/addon-storyshots-puppeteer';
+import puppeteer, { Page, Browser } from 'puppeteer';
 
-const getMatchOptions = ({ context }: { context: Context }) => {
-  const snapshotPath = join(
-    'test',
-    '__snapshots__',
-    context.kind,
-    context.story
-  );
+const HOST_IP = process.env.HOST_IP;
+const VIEWPORT = { width: 1280, height: 720, deviceScaleFactor: 2 };
+let browser: Browser;
 
-  return { customSnapshotsDir: snapshotPath };
-};
+const testFn = imageSnapshot({
+  storybookUrl: 'file:///opt/storybook-static',
+  getCustomBrowser: async () => {
+    browser = await puppeteer.connect({
+      browserWSEndpoint: (() => {
+        // For CI env's: If running in docker inside docker, you'll need the IP address of the host
+        if (HOST_IP) {
+          return `ws://${HOST_IP}:9222`;
+        }
+        return 'ws://localhost:9222';
+      })(),
+    });
+    return browser;
+  },
+  beforeScreenshot: async (page: Page) => {
+    await new Promise((res) => setTimeout(res, 6000));
+    await page.setViewport(VIEWPORT);
+  },
+  getScreenshotOptions: () => {
+    return {
+      encoding: 'base64',
+      fullPage: false,
+    };
+  },
+  getMatchOptions: ({ context }: { context: Context }) => {
+    const snapshotPath = join('test', '__snapshots__', context.kind);
 
-// Delays snapshot, mostly for CI
-const beforeScreenshot = () => {
-  return new Promise<void>((resolve) =>
-    setTimeout(() => {
-      resolve();
-    }, 500)
-  );
+    return { customSnapshotsDir: snapshotPath };
+  },
+});
+
+// Override 'afterAll' so jest doesn't hang
+testFn.afterAll = async () => {
+  if (browser) {
+    browser.close();
+  }
 };
 
 initStoryshots({
-  // integrityOptions: { cwd: join(__dirname, 'packages', 'component') },
-  // configPath: join(__dirname, '.storybook'),
-  test: imageSnapshot({
-    beforeScreenshot,
-    getMatchOptions,
-  }),
+  test: testFn,
 });
-
-// import initStoryshots from '@storybook/addon-storyshots';
-// import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
-// import puppeteer, { Page, Browser } from 'puppeteer';
-
-// const HOST_IP = process.env.HOST_IP;
-// const VIEWPORT = { width: 1280, height: 720, deviceScaleFactor: 2 };
-// let browser: Browser;
-
-// const testFn = imageSnapshot({
-//   storybookUrl: 'file:///opt/storybook-static',
-//   getCustomBrowser: async () => {
-//     browser = await puppeteer.connect({
-//       browserWSEndpoint: (() => {
-//         // For CI env's: If running in docker inside docker, you'll need the IP address of the host
-//         if (HOST_IP) {
-//           return `ws://${HOST_IP}:9222`;
-//         }
-//         return 'ws://localhost:9222';
-//       })(),
-//     });
-//     return browser;
-//   },
-//   beforeScreenshot: async (page: Page) => {
-//     await new Promise((res) => setTimeout(res, 6000));
-//     // const fullPage = await page.$('#root > *');
-//     const fullPage = await page.$('body');
-//     const data = await page.evaluate(() => document.documentElement.outerHTML);
-
-//     console.log(data);
-//     if (fullPage === null) {
-//       return Promise.reject(new Error('No root element found'));
-//     }
-//     const fullPageSize = await fullPage.boundingBox();
-//     await page.setViewport({
-//       ...VIEWPORT,
-//       height: fullPageSize ? ~~fullPageSize.height : VIEWPORT.height,
-//     });
-//   },
-//   getScreenshotOptions: () => {
-//     return {
-//       encoding: 'base64',
-//       fullPage: false,
-//     };
-//   },
-// });
-
-// // Override 'afterAll' so jest doesn't hang
-// testFn.afterAll = async () => {
-//   if (browser) {
-//     browser.close();
-//   }
-// };
-
-// initStoryshots({
-//   test: testFn,
-// });
 
 // /***
 
