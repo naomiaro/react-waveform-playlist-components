1a5b3e432f7629182bf19ca8f958a5f170445fde
Naomi Aro <naomiaro@gmail.com>
2022-03-20 00:30:16 -0700

    try screenshots

---
+++ test/image-snapshots.runner.ts
@@ -0,0 +1,5 @@
+import { initImageSnapshots } from '@spring-media/storybook-addon-image-snapshots';
+
+initImageSnapshots();
+
+// https://github.com/storybookjs/storybook/issues/7587#issuecomment-667481706
--- test/storyshots.test.ts
+++ test/storyshots.test.ts
@@ -1,10 +1,28 @@
 import initStoryshots from '@storybook/addon-storyshots';
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
+import { Page } from 'puppeteer';
+import path from 'path';
+
+// https://github.com/storybookjs/storybook/issues/5132#issuecomment-736414609
+// const beforeScreenshot = (page: Page) =>
+//   page.$('#root > *').then((root) => root ?? undefined);
+
+const beforeScreenshot = () => {
+  return new Promise<void>((resolve) =>
+    setTimeout(() => {
+      resolve();
+    }, 600)
+  );
+};
+
+const customizePage = (page: Page) =>
+  page.setViewport({ width: 1920, height: 1080 });
 
 initStoryshots({
   suite: 'Image storyshots',
   test: imageSnapshot({
-    // storybookUrl: `file://${path.resolve(__dirname, 'storybook-static')}`,
-    storybookUrl: 'http://localhost:6006',
+    storybookUrl: `file://${path.resolve(__dirname, '../storybook-static')}`,
+    beforeScreenshot,
+    customizePage,
   }),
 });
